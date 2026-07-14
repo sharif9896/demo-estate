b6017ea3cedbc20b441e49131e67dd169e4fd01e
sharif rayan <sharifrayan952@gmail.com>
2026-07-14 20:33:40 +0530

    Done

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: `${import.meta.env.VITE_API_URL}/api` });
+const api = axios.create({ baseURL: `https://demo-estate.onrender.com/api` });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('almas_admin_token');
